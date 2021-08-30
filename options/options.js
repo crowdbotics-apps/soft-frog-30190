ed44f9f35e91be5663536b9b0b08389cf2a1a809
Crowdbotics <team@crowdbotics.com>
2021-08-30 03:20:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "soft_frog_30190Identifier",
+  url: "https://soft_frog_30190Identifier.botics.co",
+  api: "https://soft_frog_30190Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
